e42558a7dd7954f6cfc389ccd8331f334f33a07d
Pete Hodgson <git@thepete.net>
2012-07-22 08:03:19 -0700

    add diagnostic logging on what APP_BUNDLE_PATH is set to

--- 2012 Olympics/Frank/features/support/env.rb
+++ 2012 Olympics/Frank/features/support/env.rb
@@ -6,3 +6,4 @@ Frank::Cucumber::FrankHelper.use_shelley_from_now_on
 # This constant must be set to the full, absolute path for your Frankified target's app bundle.
 # See the "Given I launch the app" step definition in launch_steps.rb for more details
 APP_BUNDLE_PATH = ENV['APP_BUNDLE_PATH'] || File.expand_path( '../../../frankified_build/Frankified.app', __FILE__ )
+puts "APP_BUNDLE_PATH is #{APP_BUNDLE_PATH}"
